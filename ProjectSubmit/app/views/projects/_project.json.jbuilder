ab0ef96b2c4e48319c892447c1837fc37dbf4bd8
Pedro Matos <pedromatos@outlook.pt>
2016-12-01 19:34:18 +0000

    Criar estrutura Projectos
    Criação da estrutura de projectos e base de dados….

---
+++ ProjectSubmit/app/views/projects/_project.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! project, :id, :title, :resumo, :data, :file, :tag_id, :ClaSchool_id, :School_id, :User_id, :created_at, :updated_at
+json.url project_url(project, format: :json)
---
+++ ProjectSubmit/db/migrate/20161201192358_create_projects.rb
@@ -0,0 +1,16 @@
+class CreateProjects < ActiveRecord::Migration[5.0]
+  def change
+    create_table :projects do |t|
+      t.string :title
+      t.text :resumo
+      t.date :data
+      t.string :file
+      t.integer :tag_id
+      t.integer :ClaSchool_id
+      t.integer :School_id
+      t.integer :User_id
+
+      t.timestamps
+    end
+  end
+end
