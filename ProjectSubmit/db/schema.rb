c2289c84c16f661d50a31a9c7a2790ebf0552821
pedroHdias <diaspedro1999@hotmail.com>
2016-12-02 19:32:54 +0000

    Criacao de Model, Controller e Views para Courses

--- ProjectSubmit/db/schema.rb
+++ ProjectSubmit/db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161201192438) do
+ActiveRecord::Schema.define(version: 20161202192535) do
+
+  create_table "courses", force: :cascade do |t|
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
