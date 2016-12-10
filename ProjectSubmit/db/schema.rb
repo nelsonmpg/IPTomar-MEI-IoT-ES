abed933d888224777a2002005e33e331f1363a99
Nelson Gomes <nelsonmpg@gmail.com>
2016-12-10 12:13:55 +0000

    no message

--- ProjectSubmit/db/schema.rb
+++ ProjectSubmit/db/schema.rb
@@ -26,12 +26,6 @@ ActiveRecord::Schema.define(version: 20161208161152) do
     t.datetime "updated_at",  null: false
   end
 
-  create_table "homes", force: :cascade do |t|
-    t.string   "index"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "projects", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at",   null: false
