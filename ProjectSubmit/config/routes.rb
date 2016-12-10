41815d14a8fad574dc877a99b604c8842bb66dc3
pedroHdias <diaspedro1999@hotmail.com>
2016-12-10 13:48:07 +0000

    conflit

--- ProjectSubmit/config/routes.rb
+++ ProjectSubmit/config/routes.rb
@@ -1,18 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users
+
   resources :projects
   resources :schools
   resources :documents
   resources :courses
-<<<<<<< HEAD
   resources :subjects
-  resources :persons
+  resources :people
   resources :persontypes
-  
-=======
+
+# devise_for :users
+# <<<<<<< HEAD  
+# =======
   #users
   #root to: 'home#index'
   #devise_for :users, controllers: { sessions: 'users/sessions' }
->>>>>>> refs/remotes/origin/master
+# >>>>>>> refs/remotes/origin/master
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
