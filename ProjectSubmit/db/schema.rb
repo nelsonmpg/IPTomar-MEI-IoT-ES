8341970cf848f41ff0a005fcf59e725b36bd6c3a
Nelson Gomes <nelsonmpg@gmail.com>
2016-12-10 15:14:31 +0000

    no message

--- ProjectSubmit/db/schema.rb
+++ ProjectSubmit/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161208161152) do
+ActiveRecord::Schema.define(version: 20161208115054) do
 
   create_table "courses", force: :cascade do |t|
     t.text     "description"
