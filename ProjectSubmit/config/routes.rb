95bdcb29fdf55dd372e790c9e340c1d4a5f78e6a
Miguel Coelho <miguel_coelho_1@hotmail.com>
2016-12-17 16:31:55 +0000

    Inicio de página para os projetos

--- ProjectSubmit/config/routes.rb
+++ ProjectSubmit/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+root to: 'projects#index'
 
   devise_for :users
   resources :projects
@@ -15,7 +16,6 @@ Rails.application.routes.draw do
   
 
   #users
-  #root to: 'home#index'
   #devise_for :users, controllers: { sessions: 'users/sessions' }
 # >>>>>>> refs/remotes/origin/master
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
