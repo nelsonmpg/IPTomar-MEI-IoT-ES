c2b687408df5b6ddf60309578b492b193e0381fa
Vasco Marques <vasco_marques_ei@hotmail.com>
2016-12-25 14:35:37 +0000

    Migração da tabela tags
    Install migrations
    
    # For the latest versions :
    rake acts_as_taggable_on_engine:install:migrations
    Review the generated migrations then migrate :
    
    rake db:migrate

--- ProjectSubmit/db/schema.rb
+++ ProjectSubmit/db/schema.rb
@@ -32,7 +32,6 @@ ActiveRecord::Schema.define(version: 20161219222232) do
     t.text     "name"
     t.integer  "project_id"
     t.index ["project_id"], name: "index_documents_on_project_id"
-
   end
 
   create_table "people", force: :cascade do |t|
@@ -90,6 +89,31 @@ ActiveRecord::Schema.define(version: 20161219222232) do
     t.datetime "updated_at",  null: false
     t.integer  "course_id"
     t.index ["course_id"], name: "index_subjects_on_course_id"
+  end
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer  "tag_id"
+    t.string   "taggable_type"
+    t.integer  "taggable_id"
+    t.string   "tagger_type"
+    t.integer  "tagger_id"
+    t.string   "context",       limit: 128
+    t.datetime "created_at"
+    t.index ["context"], name: "index_taggings_on_context"
+    t.index ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+    t.index ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context"
+    t.index ["taggable_id", "taggable_type", "tagger_id", "context"], name: "taggings_idy"
+    t.index ["taggable_id"], name: "index_taggings_on_taggable_id"
+    t.index ["taggable_type"], name: "index_taggings_on_taggable_type"
+    t.index ["tagger_id", "tagger_type"], name: "index_taggings_on_tagger_id_and_tagger_type"
+    t.index ["tagger_id"], name: "index_taggings_on_tagger_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string  "name"
+    t.integer "taggings_count", default: 0
+    t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
   create_table "users", force: :cascade do |t|
