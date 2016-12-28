5caf2fb6592d5876cc535b753711bb18d12bd191
Nelson Gomes <nelsonmpg@gmail.com>
2016-12-28 15:13:00 +0000

    Otimização do código nos cursos e nas escolas, redução do c+odigo repetido.

--- ProjectSubmit/app/assets/javascripts/application.js
+++ ProjectSubmit/app/assets/javascripts/application.js
@@ -14,5 +14,8 @@
 //= require tether
 //= require bootstrap-sprockets
 //= require bootstrap-select
+//= require 1-jquery-2.1.1.min
+//= require 2-imagelightbox
+//= require global.min
 //= require jquery_ujs
 //= require turbolinks
