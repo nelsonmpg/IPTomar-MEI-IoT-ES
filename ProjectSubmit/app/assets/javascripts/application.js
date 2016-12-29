4980062df0fede8a7773d2e310b95b578d609e24
Nelson Gomes <nelsonmpg@gmail.com>
2016-12-29 23:55:38 +0000

    Pequenos ajustes.

--- ProjectSubmit/app/assets/javascripts/application.js
+++ ProjectSubmit/app/assets/javascripts/application.js
@@ -10,12 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require 1-jquery-2.1.1.min
 //= require tether
 //= require bootstrap-sprockets
 //= require bootstrap-select
-//= require 1-jquery-2.1.1.min
-//= require 2-imagelightbox
 //= require global.min
 //= require jquery_ujs
 //= require turbolinks
+//= require 2-imagelightbox
