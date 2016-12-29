7f801a0a3c34ea695cea29fdb22660e3af5ba468
Pedro Matos <pedromatos@outlook.pt>
2016-12-29 18:11:38 +0000

    corrigido erro Sign_out
    Corrigir erro do Sign_out da pagina do projecto que dava erro na route….

--- ProjectSubmit/config/routes.rb
+++ ProjectSubmit/config/routes.rb
@@ -13,14 +13,7 @@ Rails.application.routes.draw do
   resources :institutions
   resources :presentations
 
-
-
   resources :tags, except: :show
   get 'tags/:tag', to: 'projects#index'
   #root 'projects#index'
-
-  #users - existe um erro na route quando se faz Sign_out no index do projects
-  #devise_for :users, controllers: { sessions: 'users/sessions' }
-# >>>>>>> refs/remotes/origin/master
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
