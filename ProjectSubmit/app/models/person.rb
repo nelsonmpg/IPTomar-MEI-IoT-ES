30bda8a9a41d72a828a6447c49618f26587224ab
evelyn <paulo.caria@gmail.com>
2017-01-08 18:41:09 +0000

    Validação de Ficheiros Permitidos e Tamanho
    Usar validações correctamente para tamanho, dimensão e tipo de ficheiros
    permitido

--- ProjectSubmit/app/models/person.rb
+++ ProjectSubmit/app/models/person.rb
@@ -8,9 +8,11 @@ class Person < ApplicationRecord
     has_and_belongs_to_many :supervised_projects, :class_name => "Project", :join_table => :projects_supervisors
     has_and_belongs_to_many :juried_presentations, :class_name => "Presentation", :join_table => :juries_presentations
 
-    has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "missing.png"
-    validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
-
-
+    has_attached_file :photo, 
+    		styles: { medium: "300x300>", thumb: "100x100>" }, 
+    		default_url: "missing.png"
+    validates_attachment_content_type :photo, 
+    		content_type: [/\Aimage\/.*\Z/ ,"image/jpg", "image/jpeg", "image/png", "image/gif"],
+    		size: { in: 0..500.kilobytes }
      
 end
