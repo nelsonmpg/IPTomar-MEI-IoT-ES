809fe27f82fbd7eb49885d88413a4545d9e58f25
evelyn <paulo.caria@gmail.com>
2017-01-15 20:52:50 +0000

    Alterações em Person's
    Layout's e Models

--- ProjectSubmit/app/models/person.rb
+++ ProjectSubmit/app/models/person.rb
@@ -10,7 +10,7 @@ class Person < ApplicationRecord
 
     has_attached_file :photo, 
             styles: { medium: "300x300>", thumb: "100x100>" }, 
-            default_url: "missing.png"
+            default_url: "/img/person.png"
     validates_attachment_content_type :photo, 
             content_type: [/\Aimage\/.*\Z/ ,"image/jpg", "image/jpeg", "image/png", "image/gif"]
 
