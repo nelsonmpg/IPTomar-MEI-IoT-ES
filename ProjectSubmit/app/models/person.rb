fb5d05d6f2c80de8155efd92528edab5b39616ae
evelyn <paulo.caria@gmail.com>
2017-01-15 18:53:24 +0000

    Merge remote-tracking branch 'refs/remotes/origin/master' into cariav3
    # Conflicts:
    #	ProjectSubmit/app/models/person.rb
    #	ProjectSubmit/app/views/people/show.html.erb

--- ProjectSubmit/app/models/person.rb
+++ ProjectSubmit/app/models/person.rb
@@ -8,6 +8,11 @@ class Person < ApplicationRecord
     has_and_belongs_to_many :supervised_projects, :class_name => "Project", :join_table => :projects_supervisors
     has_and_belongs_to_many :juried_presentations, :class_name => "Presentation", :join_table => :juries_presentations
 
-    has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/img/person.png"
-    validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
+    has_attached_file :photo, 
+            styles: { medium: "300x300>", thumb: "100x100>" }, 
+            default_url: "missing.png"
+    validates_attachment_content_type :photo, 
+            content_type: [/\Aimage\/.*\Z/ ,"image/jpg", "image/jpeg", "image/png", "image/gif"]
+
+    validates_attachment_size :photo, :less_than => 0.5.megabytes
 end
