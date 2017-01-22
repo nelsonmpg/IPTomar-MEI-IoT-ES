c5880f52620662a09711bcb8c78a4f1dd4806585
Vasco Marques <vasco_marques_ei@hotmail.com>
2017-01-22 15:22:30 +0000

    Twitter APP Key

--- ProjectSubmit/app/controllers/twitter_controller.rb
+++ ProjectSubmit/app/controllers/twitter_controller.rb
@@ -1,6 +1,7 @@
 class TwitterController < ApplicationController
 	def initialize
     	@client = Twitter::REST::Client.new do |config|
+      		config.consumer_key = "3jNJkFO8Q6rGwWaZjCmFzwdcT"
       		config.consumer_secret = "kzf7Q9pdJMf92bh4y7N8Up5fYOYqxKVInFeLVgdt7APtxKOYet"
       		config.access_token = "1446521881-gEU31fUevncy0Ikf2FkHQMVyZRnB9WTLobZh6o9"
       		config.access_token_secret = "4qRRUvHWYCKFQMiGmM7xe3eRj7MSKXOF4XpwqdJulumm1"
